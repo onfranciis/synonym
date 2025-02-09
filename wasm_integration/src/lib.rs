2356152322a4ae66dbdebc164046b191cba348c2
onfranciis <onukwuf@gmail.com>
2025-02-09 15:03:33 +0100

    feat: set up base wasm

---
+++ wasm_integration/src/lib.rs
@@ -0,0 +1,10 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn fibonacci(n: u32) -> u64 {
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fibonacci(n - 1) + fibonacci(n - 2),
+    }
+}
